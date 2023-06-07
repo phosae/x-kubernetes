a749dab8c54bd10c984139c062801458b91e5c11
Zeng Xu <zengxu_121@126.com>
2023-06-07 04:13:38 +0000

    api: update codegen ➡️ v1.28.0-alpha.1

--- api/hack/update-codegen-docker.sh
+++ api/hack/update-codegen-docker.sh
@@ -1,17 +1,23 @@
+#!/usr/bin/env bash
+set -eu
 PROJECT_PACKAGE=github.com/phosae/x-kubernetes/api
 
 docker run -it --rm -u root \
+    -v $(cd ../ && pwd):/go/src/github.com/phosae/x-kubernetes\
+    -v ${PWD}/hack/custom-boilerplate.go.txt:/tmp/fake-boilerplate.txt \
     -e GOPROXY="https://goproxy.cn"\
-    -e CODEGEN_PKG=/go/src/k8s.io/code-generator\
-    -v ${PWD}:/go/src/${PROJECT_PACKAGE}\
-    -w /go/src/${PROJECT_PACKAGE}\
-    zengxu/kube-code-generator:v1.27.1\
-    ./hack/update-codegen.sh
+    -e PROJECT_PACKAGE=${PROJECT_PACKAGE} \
+    -e CLIENT_GENERATOR_OUT=${PROJECT_PACKAGE}/generated \
+    -e APIS_ROOT=${PROJECT_PACKAGE} \
+    -e GENERATION_TARGETS="helpers,client,openapi" \
+    -e WITH_APPLYCONFIG="true" \
+    -e WITH_WATCH="" \
+    zengxu/kube-code-generator:v1.28.0-alpha.1
 
 docker run -it --rm -u root \
     -e GOPROXY="https://goproxy.cn"\
     -v ${PWD}/hack/custom-boilerplate.go.txt:/tmp/fake-boilerplate.txt \
     -v ${PWD}:/go/src/${PROJECT_PACKAGE}\
     -w /go/src/${PROJECT_PACKAGE}\
-    zengxu/kube-code-generator:v1.27.1-pb\
+    zengxu/kube-code-generator:v1.28.0-alpha.1\
     update-generated-protobuf.sh github.com/phosae/x-kubernetes/api/hello.zeng.dev/v1
